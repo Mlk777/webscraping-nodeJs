f80752c0f499d29a4e3c4cc1793f906c641eac2d
Mlk777 <>
2020-03-30 11:40:17 +0200

    Styling and code cleaning

--- server.js
+++ server.js
@@ -4,54 +4,48 @@ const express = require('express');
 const rp = require('request-promise');
 const ejs = require('ejs');
 const app = express();
+const PORT = process.env.port || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname, +'/styles'));
 
-app.get('/', (req, res) => {
-  rp(url)
-    .then(data => {
-      //Topics
-      let topics = [];
-      $('div > .f3', data).each(function() {
-        let topic = $(this).text();
-        topics.push(topic);
-      });
-      // console.log(topics);
+app.get('/', async (req, res) => {
+  try {
+    const data = await rp(url);
+    const topics = [];
+    const descriptions = [];
+    const links = [];
 
-      //Descriptions
-      let descriptions = [];
-      $('li > a > div > div > .f5', data).each(function() {
-        let description = $(this).text();
-        descriptions.push(description);
-      });
-      // console.log(descriptions);
-
-      //Links
-      let links = [];
-      $('.py-4 > a', data).each(function() {
-        let link = $(this).attr('href');
-        links.push(link);
-      });
-      // console.log(descriptions);
-
-      let result = topics.map((topic, index) => {
-        return {
-          topic: topic,
-          description: descriptions[index],
-          link: `https://github.com${links[index]}`
-        };
-      });
-
-      // console.log(result);
-
-      res.render('index', { result: result });
-    })
-    .catch(err => {
-      console.log(err);
+    //Topics
+    $('div > .f3', data).each((i, topic) => {
+      topics.push($(topic).text());
     });
+
+    //Descriptions
+    $('div > .f5.text-gray', data).each((i, description) => {
+      descriptions.push($(description).text());
+    });
+    console.log(descriptions);
+
+    //Links
+    $('.py-4 > a', data).each((i, link) => {
+      links.push($(link).attr('href'));
+    });
+
+    const result = topics.map((topic, index) => {
+      return {
+        topic,
+        description: descriptions[index],
+        link: `https://github.com${links[index]}`
+      };
+    });
+
+    res.render('index', { result });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-app.listen(3000, () => {
-  console.log('The server is live !');
+app.listen(PORT, () => {
+  console.log(`The server is live on port ${PORT} !`);
 });
